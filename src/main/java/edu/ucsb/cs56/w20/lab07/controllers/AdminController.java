3ca6d956509bd17fda2deff03334955133bb3be6
Phill Conrad <pconrad@cs.ucsb.edu>
2020-02-10 13:43:39 -0800

    bt/pc - Protect admin page from access by non-admins

--- src/main/java/edu/ucsb/cs56/w20/lab07/controllers/AdminController.java
+++ src/main/java/edu/ucsb/cs56/w20/lab07/controllers/AdminController.java
@@ -1,12 +1,27 @@
 package edu.ucsb.cs56.w20.lab07.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.ucsb.cs56.w20.lab07.services.MembershipService;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    private MembershipService ms;
+
     @GetMapping("/admin")
-    public String admin(){
+    public String admin(OAuth2AuthenticationToken token, RedirectAttributes redirAttrs){
+        String role = ms.role(token);
+
+        if (! role.equals("Admin")) {
+            redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
+            return "redirect:/";
+        }
         return "admin/create";
     }
 }
