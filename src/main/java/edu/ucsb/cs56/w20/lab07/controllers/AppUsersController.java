1a1fa73082f19227223939c2e45a0edb44e5dbd8
Phill Conrad <pconrad@cs.ucsb.edu>
2020-02-11 15:06:44 -0800

    pc - add users to nav and ability to delete users

--- src/main/java/edu/ucsb/cs56/w20/lab07/controllers/AppUsersController.java
+++ src/main/java/edu/ucsb/cs56/w20/lab07/controllers/AppUsersController.java
@@ -1,7 +1,11 @@
 package edu.ucsb.cs56.w20.lab07.controllers;
 
+import edu.ucsb.cs56.w20.lab07.entities.AppUser;
 import edu.ucsb.cs56.w20.lab07.repositories.AppUserRepository;
 import edu.ucsb.cs56.w20.lab07.services.MembershipService;
+
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,8 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -39,9 +45,8 @@ public class AppUsersController {
         return "users/index";
     }
 
-    /*
-    @PostMapping("/admin/delete/{id}")
-    public String deleteAdmin(@PathVariable("id") long id, Model model,
+    @PostMapping("/users/delete/{id}")
+    public String deleteAdmin(@PathVariable("id") Long id, Model model,
             RedirectAttributes redirAttrs, OAuth2AuthenticationToken token) {
         String role = ms.role(token);
         if (!role.equals("Admin")) {
@@ -50,59 +55,23 @@ public class AppUsersController {
             return "redirect:/";
         }
 
-        Optional<Admin> admin = appUserRepository.findById(id);
-        if (!admin.isPresent()) {
-            redirAttrs.addFlashAttribute("alertDanger", "Admin with that id does not exist.");
+        Optional<AppUser> optionalAppUser = appUserRepository.findById(id);
+        if (!optionalAppUser.isPresent()) {
+            redirAttrs.addFlashAttribute("alertDanger", "User with that id does not exist.");
         } else {
-            String email = admin.get().getEmail();
-            if (ms.getAdminEmails().contains(email)) {
-                redirAttrs.addFlashAttribute("alertDanger", "Admin " + email + " was set from application properties and cannot be deleted.");
+            AppUser appUser = optionalAppUser.get();
+            String email = appUser.getEmail();
+            String curEmail = ms.email(token);
+
+            if (email.equals(curEmail)) {
+                redirAttrs.addFlashAttribute("alertDanger", "Cannot delete the current user");
             } else {
-                appUserRepository.delete(admin.get());
-                redirAttrs.addFlashAttribute("alertSuccess", "Admin successfully deleted.");
+                appUserRepository.delete(appUser);
+                redirAttrs.addFlashAttribute("alertSuccess", "User " + email + "successfully deleted.");
             }
         }
-        model.addAttribute("newAdmin", new Admin());
-        model.addAttribute("admins", appUserRepository.findAll());
-        return "redirect:/admin";
+        model.addAttribute("users", appUserRepository.findAll());
+        return "redirect:/users";
     }
 
-    @PostMapping("/admin/add")
-    public String addAdmin(@Valid Admin admin, BindingResult result, Model model,
-            RedirectAttributes redirAttrs, OAuth2AuthenticationToken token) {
-        String role = ms.role(token);
-        if (!role.equals("Admin")) {
-            redirAttrs.addFlashAttribute("alertDanger",
-                    "You do not have permission to access that page");
-            return "redirect:/";
-        }
-
-        boolean errors = false;
-        if (!ValidEmailService.validEmail(admin.getEmail())) {
-            errors = true;
-            redirAttrs.addFlashAttribute("alertDanger", "Invalid email.");
-        }
-        List<Admin> alreadyExistingAdmins = appUserRepository.findByEmail(admin.getEmail());
-        if (!alreadyExistingAdmins.isEmpty()) {
-            errors = true;
-            redirAttrs.addFlashAttribute("alertDanger", "An admin with that email already exists.");
-        }
-        if (!errors) {
-            appUserRepository.save(admin);
-            model.addAttribute("newAdmin", new Admin());
-        } else {
-            model.addAttribute("newAdmin", admin);
-        }
-        model.addAttribute("admins", appUserRepository.findAll());
-        return "redirect:/admin";
-    }
-
-    private void addAdminsFromPropertiesFile() {
-        ms.getAdminEmails().forEach((email) -> {
-            if (appUserRepository.findByEmail(email).isEmpty()) {
-                appUserRepository.save(new Admin(email, true));
-            }
-        });
-    }
-    */
 }
